00f4b5b73dc867b293d59b5012012fd6be459548
Achint Singh <achintsingh97@gmail.com>
2018-09-02 23:53:57 -0700

    Texts are automated to send at a certain time

--- send_sms.js
+++ send_sms.js
@@ -4,15 +4,23 @@ const accountSid = process.env.ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
+var schedule = require('node-schedule');
+var rule = new schedule.RecurrenceRule();
+rule.dayOfWeek = [0, new schedule.Range(0, 6)];
+rule.hour = 9;
+rule.minute = 0;
+
+schedule.scheduleJob(rule, function(){
+  // need client messages to be called here
+  client.messages
   .create({
      body: `${randomMessage()}`,
-     from: process.env.SENDER,
+     from: proccess.env.SENDER,
      to: process.env.RECEIVER
    })
   .then(message => console.log(message.sid))
   .done();
-
+});
 
 function randomMessage() {
   var messages = [
@@ -21,7 +29,8 @@ function randomMessage() {
       'I hope you have a good day!', 
       'How is your morning going?',
       'What are you up to today?',
-      'How are you doing today?' ];
+      'How are you doing today?' 
+    ];
 
   var randomNum = Math.floor(Math.random()*messages.length);
   return messages[randomNum];
